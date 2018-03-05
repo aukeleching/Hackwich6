fde059d33876b96f9441943fce3badc83f2cf78f
CM Student <cmstudent@UHWO-ACM-C-02.local>
2018-03-05 10:46:39 -1000

    Completed entire hackwich 6.

--- Hackwich6/ViewController.swift
+++ Hackwich6/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var slider: UISlider!
+    var currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +25,20 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func sliderHasMoved(_ sender: Any) {
+        print("The value of the slider is:\(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    @IBAction func myGuessButtonPressed(_ sender: Any) {
+        let message = "The value is: \(currentValue)"
+        
+        let alert = UIAlertController(title:"Hello World!", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
 
