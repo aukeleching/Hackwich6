58c96514ca815fef2245c809d6d8d75f0ea7de5c
CM Student <cmstudent@UHWO-ACM-C-02.local>
2018-03-12 10:44:37 -1000

    Completed part4 step three.

--- Hackwich6/ViewController.swift
+++ Hackwich6/ViewController.swift
@@ -10,15 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet var slider: UISlider!
+    //create a variable that can hold the value of the slider
     var currentValue: Int = 0
+    
+    //create a variable to hold the target value
+    var targetValue: Int = 0
+    
+    
+    @IBOutlet var targetLabel: UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Set currentValue to the intial value of the slider
+        currentValue = lroundf(slider.value)
+        
+        //call startNewRound
+        startNewRound()
+        
+        
     }
 
+    func updateTargetLabel(){
+        targetLabel.text = String(targetValue)
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -29,8 +50,27 @@ class ViewController: UIViewController {
         print("The value of the slider is:\(slider.value)")
         currentValue = lroundf(slider.value)
     }
+    
+    func startNewRound() {
+        
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateTargetLabel() 
+    }
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
     @IBAction func myGuessButtonPressed(_ sender: Any) {
-        let message = "The value is: \(currentValue)"
+        //5. New variable message that displays a message that includes currentValue
+        let message = "The value is: \(currentValue)" + "\nThe target value is:\(targetValue)"
         
         let alert = UIAlertController(title:"Hello World!", message: message, preferredStyle: .alert)
         
@@ -39,6 +79,8 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
 }
 
